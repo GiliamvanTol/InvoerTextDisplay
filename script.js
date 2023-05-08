eb0548fbedfdcda65e5e17b020a8db3fcc7304cf
Giliam <36519691+GiliamvanTol@users.noreply.github.com>
2023-05-08 21:35:45 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const invoer = document.getElementById("invoer")
+const hoofd = document.getElementById("hoofd")
+
+invoer.addEventListener("input", show)
+
+function show(){
+
+if(invoer.value){
+  
+hoofd.innerText = invoer.value + " is cool"
+
+} else{
+
+hoofd.innerText = "Invoer Text Display"
+}
+
+}
+
